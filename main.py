afc954a123140ee42a9e3e8139c47cbd51be0815
Bernd Schlütter <bernd.schluetter@hss-wiesloch.de>
2023-12-19 09:26:37 +0100

    Verbindungsdaten aus config.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
+
+import config
 from models import db, Coffee
 from mongoengine.queryset.visitor import Q
-import config
+
 
 def create_app():
     flask_app = Flask(__name__)
@@ -11,23 +13,26 @@ def create_app():
 
     # connection to MongoDB
     flask_app.config['MONGODB_SETTINGS'] = {
-        'db': 'coffeebase',
-        'host': 'localhost',
-        'port': 27017,
-        'username': 'coffee',
-        'password': 'coffee125',
-        'authentication_source': 'coffeebase'
+        'db': config.MONGO_DB,
+        'host': config.MONGO_HOST,
+        'port': config.MONGO_PORT,
+        'username': config.MONGO_USERNAME,
+        'password': config.MONGO_PASSWORD,
+        'authentication_source': config.MONGO_AUTHENTIFICATION_SOURCE
     }
 
     db.init_app(flask_app)
 
     return flask_app
 
+
 app = create_app()
+
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
 
 @app.route('/index/<cof_user_id>')
 def index(cof_user_id):
@@ -35,7 +40,9 @@ def index(cof_user_id):
     coffees = Coffee.objects(cof_user_id=cof_user_id).order_by('-cof_date')
     counter_false = Coffee.objects(Q(cof_user_id=cof_user_id) & Q(cof_payed=False)).count()
     counter_all = Coffee.objects(cof_user_id=cof_user_id).count()
-    return render_template('index.html', coffees=coffees, counter_false=counter_false, counter_all=counter_all, user=cof_user_id)
+    return render_template('index.html', coffees=coffees, counter_false=counter_false, counter_all=counter_all,
+                           user=cof_user_id)
+
 
 @app.route('/add_coffee/<cof_user_id>')
 def add_coffee(cof_user_id):
@@ -47,6 +54,7 @@ def add_coffee(cof_user_id):
         return redirect(url_for('index', cof_user_id=cof_user_id))
     except Exception as e:
         return f"An error occurred: {str(e)}"
+
 
 @app.route('/pay_coffee/<cof_user_id>')
 def pay_coffee(cof_user_id):
@@ -63,7 +71,3 @@ if __name__ == '__main__':
     print('Direkter Start über Console mit [python3 main.py]')
     port = app.config.get('PORT')
     app.run(host=app.config.get('HOST'), debug=app.config.get('DEBUG'), port=app.config.get('PORT'))
-
-else:
-    print('Start von NGINX Webserver')
-    app.run(debug=config.DEBUG)
