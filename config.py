81ce41e18e419a228743224ced512dfd028a0050
Bernd Schlütter <bernd.schluetter@hss-wiesloch.de>
2023-12-19 09:27:36 +0100

    Zugangsdaten MongoDB

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 # config.py
-DEBUG=False
+DEBUG = False
+
+MONGO_DB = 'coffeebase'
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'coffee'
+MONGO_PASSWORD = 'coffee125'
+MONGO_AUTHENTIFICATION_SOURCE = 'coffeebase'
