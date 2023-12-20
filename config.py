06511a706b2898d055521371b14e6936f53c7a6f
Bernd Schlütter <bernd.schluetter@hss-wiesloch.de>
2023-12-20 19:14:33 +0100

    config.py nicht un GITHUB

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-DEBUG = False
-
-MONGO_DB = 'coffeebase'
-MONGO_HOST = 'artus'
-MONGO_PORT = 27017
-MONGO_USERNAME = 'coffee'
-MONGO_PASSWORD = 'coffee125'
-MONGO_AUTHENTIFICATION_SOURCE = 'coffeebase'
